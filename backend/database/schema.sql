af73b48ad3330ebfeb41c46f060100ca58d9e667
mguittari <mattias.guittari@gmail.com>
2024-03-27 13:11:11 +0100

    presentation and schema ok

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,26 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+create table user (
+  id INT PRIMARY_KEY AUTO_INCREMENT,
+  pseudo VARCHAR(20) NOT NULL,
+  email VARCHAR(50) UNIQUE,
+  hashed_password VARCHAR(250) NOT NULL,
+  role NOT NULL DEFAULT "user",
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table comment (
+  id INT PRIMARY_KEY AUTO_INCREMENT,
+  title VARCHAR(20) NOT NULL,
+  content VARCHAR(1000) NOT NULL,
+  id_user INT NOT NULL,
+  id_article INT NOT NULL,
+  FOREIGN KEY (id_user) REFERENCES user(id) ON DELETE CASCADE,
+  FOREIGN KEY (id_article) REFERENCES article(id) ON DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table article (
+id INT PRIMARY_KEY AUTO_INCREMENT,
+title VARCHAR(250) NOT NULL,
+content TEXT NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
