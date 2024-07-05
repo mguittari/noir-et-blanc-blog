7d62b40f74d878dcb9277b93175345d416b80f8f
mguittari <mattias.guittari@gmail.com>
2024-07-05 16:45:30 +0200

    state of like button stored in localstorage (true : full / false : empty) || unauthorized to like when user is deconnected || remove localstorage like key when associated comment is deleted

--- frontend/src/components/Buttons/LikeButton.jsx
+++ frontend/src/components/Buttons/LikeButton.jsx
@@ -1,18 +1,28 @@
 import PropTypes from "prop-types";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AiOutlineLike, AiFillLike } from "react-icons/ai";
 import { UserContext } from "../../context/userContext";
 
-export default function LikeButton({
-  nbLike,
-  commentId,
-  onRefreshLikeCounter,
-}) {
-  console.info("nbLike?", nbLike);
-
+export default function LikeButton({ commentId, onRefreshLikeCounter }) {
   const { token, user } = useContext(UserContext);
+  const localStorageKey = `isLiked_${commentId}_${user.user?.id_user}`;
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  useEffect(() => {
+    const storedLikeStatus = localStorage.getItem(localStorageKey);
+    if (storedLikeStatus) {
+      setIsLiked(JSON.parse(storedLikeStatus));
+    }
+  }, [localStorageKey]);
 
   const handleClick = (e) => {
     e.preventDefault();
+    if (token.message === "vous avez été déconnecté") {
+      console.warn("You are not allowed to like this comment.");
+      return;
+    }
+
     fetch(`http://localhost:3310/api/comment/${commentId}/like`, {
       method: "POST",
       headers: {
@@ -26,6 +36,9 @@ export default function LikeButton({
     })
       .then(() => {
         onRefreshLikeCounter();
+        const newIsLiked = !isLiked;
+        setIsLiked(newIsLiked);
+        localStorage.setItem(localStorageKey, JSON.stringify(newIsLiked));
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -34,15 +47,20 @@ export default function LikeButton({
 
   return (
     <div className="flex flex-col-2">
-      <button type="button" onClick={handleClick}>
-        👍
-      </button>
+      {isLiked ? (
+        <button label="unlike button" type="button" onClick={handleClick}>
+          <AiFillLike />
+        </button>
+      ) : (
+        <button label="like button" type="button" onClick={handleClick}>
+          <AiOutlineLike />
+        </button>
+      )}
     </div>
   );
 }
 
 LikeButton.propTypes = {
-  nbLike: PropTypes.number.isRequired,
   commentId: PropTypes.number.isRequired,
   onRefreshLikeCounter: PropTypes.func.isRequired,
 };
